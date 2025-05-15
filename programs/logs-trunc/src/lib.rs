272e946ff19dcdf118202471603efb75d6abd910
Georgi Ivanov <3618084+georgiIvanov@users.noreply.github.com>
2025-05-15 11:36:30 +0300

    generate logs

--- programs/logs-trunc/src/lib.rs
+++ programs/logs-trunc/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("BT29poq1aygVaHLCA7Dz64mK7nvxgCifPw9zKDXuc36v");
+declare_id!("FXjLSRRVSFJXXmvNa29RivshADPLjFxJb4g7AwbvHYWN");
 
 #[program]
 pub mod logs_trunc {
@@ -10,7 +10,24 @@ pub mod logs_trunc {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
+
+    pub fn deposit(ctx: Context<Deposit>) -> Result<()> {
+        for i in 0..11 {
+            // Create a string with repeating digits based on the iteration number
+            let digit = i.to_string();
+            let large_string = digit.repeat(1_024_000);
+            
+            // Log the large string
+            msg!("{}", large_string);
+        }
+
+        msg!("Deposit executed: amount = 1234, fee = 1, etc, etc");
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[derive(Accounts)]
+pub struct Deposit {}
